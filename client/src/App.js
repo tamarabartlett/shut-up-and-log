03a223e80951cc57d9740fb0244c7494d82ab59c
tamarabartlett <tbartlett@pivotal.io>
2018-06-22 08:51:29 -0600

    Add same jump to db eery time

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import './css/App.css';
 import LastJump from './LastJump.js';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 import Graphs from './Graphs.js';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import logo from './img/favicon.png'
+
+
+const styles = () => ({
+  fab: {
+    position: 'absolute',
+    bottom: '3em',
+    right: '3em',
+    backgroundColor: '#0ad3ff'
+  }
+});
 
 class App extends Component {
   state = {
@@ -29,7 +43,29 @@ class App extends Component {
     return body;
   };
 
+  addJump = async () => {
+    let settings = {
+       method: 'POST',
+       headers: {'Content-Type':'application/x-www-form-urlencoded'},
+       body: 'jump_number=19&jump_date=2016-10-10&type=freefly&altitude=11000&location=Elsinore'
+   };
+
+   let data = await fetch(`api/skydives`, settings)
+       .then(response => response.json())
+       .then(json => {
+           return json;
+       })
+       .catch(e => {
+           return e
+       });
+
+       console.log(data);
+       return data;
+  }
+
   render() {
+    const { classes } = this.props;
+
     return (
       <div className="App">
         <header className="App-header">
@@ -41,9 +77,20 @@ class App extends Component {
 
         <LastJump jump={this.state.lastJump}/>
         <Graphs jumps={this.state.jumps}/>
+          <Button
+            variant="fab"
+            aria-label="add"
+            className={classes.fab}
+            onClick={this.addJump}>
+            <AddIcon />
+          </Button>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(App);
