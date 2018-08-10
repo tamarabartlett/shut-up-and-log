337fedc36e808fa8e8203f5e22327fb63adc2f2d
tamarabartlett <tbartlett@pivotal.io>
2018-08-09 21:41:34 -0700

    getting header into shape:

--- client/src/NewJump.js
+++ client/src/NewJump.js
@@ -2,38 +2,45 @@ import React, { Component } from "react";
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-
-const styles = theme => ({
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-    width: 200,
-  },
-});
+//
+// const styles = theme => ({
+//   textField: {
+//     marginLeft: theme.spacing.unit,
+//     marginRight: theme.spacing.unit,
+//     width: 200,
+//   },
+// });
 
 class NewJump extends Component {
   state = {
-    dropzone: 'Orange Skies',
-    date: 'Today',
+    dropzone: '',
+    date: '',
     lastJump: '',
-    greeting: 'Hiiiii',
+    altitude: '',
+    jumpType: '',
+    jumpNumber: ''
   };
 
-  handleChange = value => event => {
-    this.setState({
-      [value]: event.target.value,
-    })
-  }
-
   getNextJumpNumber = () => {
-    return this.state.lastJump.jump_number + 1
+    return this.state.jumpNumber
   }
 
   componentDidMount() {
+    let day = new Date().getDate();
+    let month = new Date().getMonth() + 1;
+    let year = new Date().getFullYear();
+    let date = year + '-' + month + '-' + day;
+
+    this.setState({
+      date: date
+    })
+
     this.callApi()
       .then(res => {
         this.setState({
-          lastJump: res.data[res.data.length-1]
+          lastJump: res.data[res.data.length-1],
+          jumpNumber: res.data[res.data.length-1].jump_number + 1,
+          dropzone: res.data[res.data.length-1].location
          })
       }).catch(err => console.log(err));
   }
@@ -46,18 +53,55 @@ class NewJump extends Component {
     return body;
   }
 
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    })
+  }
+
+  addJump = async () => {
+    let bodyString = 'jump_number=' + this.state.jumpNumber +
+                      '&jump_date=' + this.state.date +
+                      '&type=' + this.state.jumpType +
+                      '&altitude=' + this.state.altitude +
+                      '&location=' + this.state.dropzone
+
+    console.log(bodyString)
+    let settings = {
+       method: 'POST',
+       headers: {'Content-Type':'application/x-www-form-urlencoded'},
+       body: bodyString
+   };
+
+   let data = await fetch(`api/skydives`, settings)
+       .then(response => response.json())
+       .then(json => {
+           return json;
+       })
+       .catch(e => {
+           return e
+       });
+
+       console.log(data);
+       return data;
+  }
+
   render() {
     const { classes } = this.props;
 
     return (
       <div>
         <h1>Add A Jump</h1>
-          <form className={classes.container} noValidate autoComplete="off">
+          <form
+            // className={classes.container}
+            onSubmit={this.addJump}
+            noValidate
+            autoComplete="off">
             <TextField
               id="dropzone"
               label="Dropzone"
-              className={classes.textField}
-              value={this.state.lastJump.location}
+              // className={classes.textField}
+              value={this.state.dropzone}
               onChange={this.handleChange('dropzone')}
               InputLabelProps={{
                 shrink: true,
@@ -67,7 +111,7 @@ class NewJump extends Component {
             <TextField
               id="date"
               label="Date"
-              className={classes.textField}
+              // className={classes.textField}
               value={this.state.date}
               onChange={this.handleChange('date')}
               margin="normal"
@@ -75,7 +119,7 @@ class NewJump extends Component {
             <TextField
               id="jumpNumber"
               label="Jump Number"
-              className={classes.textField}
+              // className={classes.textField}
               value={this.getNextJumpNumber()}
               onChange={this.handleChange('jumpNumber')}
               InputLabelProps={{
@@ -83,14 +127,31 @@ class NewJump extends Component {
               }}
               margin="normal"
             />
+            <TextField
+              id="altitude"
+              label="Altitude"
+              // className={classes.textField}
+              value={this.state.altitude}
+              onChange={this.handleChange('altitude')}
+              margin="normal"
+            />
+            <TextField
+              id="jumpType"
+              label="Jump Type"
+              // className={classes.textField}
+              value={this.state.jumpType}
+              onChange={this.handleChange('jumpType')}
+              margin="normal"
+            />
+            <input type="submit" value="Submit" />
           </form>
       </div>
     );
   }
 }
-
-NewJump.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(NewJump);
+//
+// NewJump.propTypes = {
+//   classes: PropTypes.object.isRequired,
+// };
+export default NewJump;
+// export default withStyles(styles)(NewJump);
